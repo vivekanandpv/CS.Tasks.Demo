d2fd2086349fe05bc3f1e9d4a9b6a625594120d5
vivek <vivek@athenaeum.in>
2021-05-12 10:46:40 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CS.Tasks.Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
