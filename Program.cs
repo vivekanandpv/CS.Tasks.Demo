8dfe5d506e21bfe3f2325f0971c41857e2f890fc
vivek <vivek@athenaeum.in>
2021-05-12 12:16:21 +0530

    done

--- Program.cs
+++ Program.cs
@@ -7,30 +7,33 @@ namespace CS.Tasks.Demo
 {
     class Program
     {
-        //  for C# 5, 6, 7
-        static void Main(string[] args)
+        //  https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/proposals/csharp-7.1/async-main
+        static async Task Main(string[] args)
         {
-
-            //  also consider the shorthand:
-            //  var result = GetThirdTaskAsync().Result;
-            var awaiter = GetThirdTaskAsync().GetAwaiter(); //  non-blocking
-
-            Console.WriteLine("Main thread continues");
-
-            var result = awaiter.GetResult();   //  blocked
-
-            Console.WriteLine($"Main thread resumes after blocking; result: {result}");
+            try
+            {
+                var result = await GetThirdTaskAsync();   //  blocked
+                Console.WriteLine($"Main thread resumes after blocking; result: {result}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Caught: {e.Message}");
+            }
         }
 
         public static async Task<int> GetThirdTaskAsync()
         {
             var resultFromFirst = await GetSecondTaskAsync();
+            //throw new Exception("Oops! thrown in the GetThirdTaskAsync");
+            Console.WriteLine($"Third task completing... Thread ID: {Thread.CurrentThread.ManagedThreadId}");
             return resultFromFirst * resultFromFirst;
         }
 
         public static async Task<int> GetSecondTaskAsync()
         {
             var resultFromFirst = await GetFirstTaskAsync();
+            //throw new Exception("Oops! thrown in the GetSecondTaskAsync");
+            Console.WriteLine($"Second task completing... Thread ID: {Thread.CurrentThread.ManagedThreadId}");
             return resultFromFirst * resultFromFirst;
         }
 
@@ -41,6 +44,7 @@ namespace CS.Tasks.Demo
             return Task.Run(() =>
             {
                 Thread.Sleep(1000);
+                throw new Exception("Oops! thrown in the GetFirstTaskAsync");
                 Console.WriteLine($"First task completing... Thread ID: {Thread.CurrentThread.ManagedThreadId}");
                 return 2;
             });
